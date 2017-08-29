831f44918fbf92472bcb260f9ef2732c4a4a4a17
Keven-H <huanjianzhong@Fengnian.cn>
2017-08-29 17:15:36 +0800

    Initial Commit

---
+++ RACTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACTest
+//
+//  Created by Admin on 2017/8/29.
+//  Copyright © 2017年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
